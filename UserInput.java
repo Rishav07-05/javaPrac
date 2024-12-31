c8803d8746c73c08cf4167d24058c26ea23db0a3
Rishav Kumar <rishavkumar18902@gmail.com>
2024-12-31 21:48:26 +0530

    Taking user input

--- UserInput.java
+++ UserInput.java
@@ -5,6 +5,15 @@ public class UserInput{
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+
+        scanner.close();
         
     }
 }
