a22319243b7a6c040f52c922bf67e504794f8b32
Rishav Kumar <rishavkumar18902@gmail.com>
2025-01-24 11:49:42 +0530

    file add as java

---
+++ Fact.java
@@ -0,0 +1,35 @@
+// import java.util.*;
+// public class Fact {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter your input: ");
+//         int n = sc.nextInt();
+//         int b = sc.nextInt();
+//         int add = n + b;
+//         System.out.println("Add: " + add);
+//         sc.close();
+//     }
+// }
+
+
+import java.util.*;
+
+public class Fact {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (n > a && n > b) {
+            System.out.println(n);
+        }
+        else if (a > n && a > b) {
+            System.out.println(a);
+        }
+        else {
+            System.out.println("greatest is : " + b);
+        }
+        sc.close();
+    }
+}
